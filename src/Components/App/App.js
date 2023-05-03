cdc6605f1f1c9a3e448a7508b66f315f31687595
Jesus Borjas <jesusbor006@gmail.com>
2023-05-02 22:25:34 -0500

    Set weather state to the data being fetched

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,8 +1,24 @@
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [weather, setWeather] = useState({});
+  console.log(weather)
+
+  useEffect(() => {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=chicago&appid=bdf2d3d4c86056684a6c8b16f1dd1be1&units=imperial`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setWeather(data)
+      });
+  }, []);
+
   return (
-   <h1>Weather App</h1>
+    <main className="main">
+      <h1 className="heading">Weather App</h1>
+    </main>
   );
 }
 
